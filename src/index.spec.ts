41141eae0fe630cc9b39f885dde0edc9725ff8bd
roman.vasilev <>
2018-06-09 14:19:41 +0400

    New: First release

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,16 @@
-import * as assert from 'assert';
+import assert = require('assert');
 import * as lib from './index';
 
 it('smoke', () => {
     assert(lib);
+    assert.equal(typeof lib.refHolder, 'function');
+});
+
+it('holds reference', () => {
+    const holder = lib.refHolder();
+    const testCallback = (value: any) => (callback: (value) => void) => {
+        callback(value);
+    };
+    testCallback('testValue')(holder);
+    assert.equal(holder.ref, 'testValue');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function hello() {
-    return 'world';
+export interface RefHolderInterface<T> {
+    (value?: T): void;
+    ref: T;
 }
+
+export const refHolder = <T = any>(): RefHolderInterface<T> => {
+    let reference: Element;
+
+    function RefHolder(ref: Element) {
+        reference = ref;
+    }
+
+    Object.defineProperty(RefHolder, 'ref', {
+        get: () => reference,
+        enumerable: true,
+        configurable: true
+    });
+
+    return RefHolder as any;
+};
